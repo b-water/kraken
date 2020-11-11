275437d4003304352d831f71477fe4b4ddb1e3a5
Nico Schmitz <bwaterxo@gmail.com>
2020-11-11 21:15:19 +0100

    added script wrapper with menu

---
+++ kraken.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "_ switching into scripts directory"
+
+cd scripts/
+
+PS3='Please enter your choice: '
+options=("Full Install" "Build Docker Container" "Setup Environment" "Import Data" "Quit")
+
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Full Install")
+            echo "you chose choice $REPLY which is $opt"
+            . build.sh
+            . setup_environment.sh
+            . import_data.sh
+            break
+            ;;
+        "Build Docker Container")
+            echo "you chose choice $REPLY which is $opt"
+            . build.sh
+            break
+            ;;
+        "Setup Environment")
+            echo "you chose choice $REPLY which is $opt"
+            . setup_environment.sh
+            break;
+            ;;
+        "Import Data")
+            echo "you chose choice $REPLY which is $opt"
+            . import_data.sh
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+echo "_ switching back to previous directory"
+
+cd -
+
+echo "_ quitting"
