b5f7606c5e9ba26989631ecf72ca6832427109f8
Nico Schmitz <bwaterxo@gmail.com>
2020-11-11 20:53:03 +0100

    fix build process

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "_ setup environment for app to run"
+docker-compose exec app cp .env.example .env
 docker-compose exec app composer install
 docker-compose exec app php artisan key:generate
-docker-compose exec app npm install && npm run dev
+docker-compose exec app npm install
+docker-compose exec app npm run dev
