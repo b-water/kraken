5d1017607e53c23f55029a4c202da3614e0ab902
b-water <bwaterxo@gmail.com>
2022-02-27 00:51:02 +0100

    reset

--- app/Enum/StatusEnum.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-final class StatusEnum {
-
-    const DEFAULT = self::INACTIVE;
-    const INACTIVE = 0;
-    const ACTIVE = 1;
-}
--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -2,6 +2,7 @@
 echo "_ setup environment for app to run"
 docker-compose exec app cp .env.example .env
 docker-compose exec app composer install
+docker-compose exec app composer self-update --2
 docker-compose exec app php artisan key:generate
 docker-compose exec app npm install
 docker-compose exec app npm run dev
